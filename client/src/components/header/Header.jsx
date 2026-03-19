3ac083deb0f32ebf801b6e02e748d10b631f7015
Danil Holikov <break.build.one.minute@gmail.com>
2026-03-19 14:52:49 +0200

    Header is finished

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,16 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../../theme/ThemeContext";
 
 export default function Header() {
+  const { themeHandler } = useContext(ThemeContext);
   return (
     <>
       <header className="header block">
         <div className="header_wrapper">
           <nav>
-            <NavLink to="/visits">
+            <NavLink to="/">
               <div className="logo">
                 <h1 className="mt-bd">KidsSmile</h1>
               </div>
@@ -16,7 +19,24 @@ export default function Header() {
             <ul className="menu_list">
               <li className="families mt-bd flex gap-[10px]">
                 <NavLink to="/families" className="header_link flex gap-[10px]">
-                  <img src="/img/drop_down.svg" alt="Drop Down" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 16.4749 9.98743"
+                    width="16.474854"
+                    height="9.987427"
+                    fill="none"
+                    customFrame="#000000"
+                  >
+                    <path
+                      id="DropDown"
+                      d="M7 14L0 7L7 0"
+                      stroke="rgb(79,81,89)"
+                      stroke-linejoin="round"
+                      stroke-width="3.500000"
+                      transform="matrix(6.12323e-17,-1,1,6.12323e-17,1.23743,8.23743)"
+                    />
+                  </svg>
                   Сім'ї
                 </NavLink>
 
@@ -28,7 +48,7 @@ export default function Header() {
               </li>
 
               <li className="visits mt-bd">
-                <NavLink to="/visits">Вiзити</NavLink>
+                <NavLink to="/">Вiзити</NavLink>
               </li>
 
               <li className="story mt-bd">
@@ -39,8 +59,26 @@ export default function Header() {
         </div>
       </header>
 
-      <div className="settings">
+      <div className="settings_block">
         <img src="/img/settings.svg" alt="Settings" />
+        <div className="settings_wrapper">
+          <div className="settings">
+            <div className="theme msh-bd">
+              <p>Тема:</p>
+              <div className="outer_toggle" onClick={themeHandler}>
+                <div className="inner_toggle"></div>
+              </div>
+            </div>
+
+            <div className="prise_per_hour msh-bd">
+              <p>Цiна/год:</p>
+              <div className="price_wrapper flex justify-between items-center gap-[4px]">
+                <img src="/img/edit.svg" alt="Edit" />
+                <p>100</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
