ac8858fac5ea762b0f9a725689aec0874d97345d
Danil Holikov <break.build.one.minute@gmail.com>
2026-04-13 17:16:41 +0300

    feat(.types.ts): refined interface type for NewFamilyInterface

--- client/src/.types.ts
+++ client/src/.types.ts
@@ -1,1 +1,15 @@
 export type Void = () => void;
+
+export interface NewFamilyInterface {
+  familyName: string;
+  adults: {
+    name: string;
+    phone: string;
+    roleId: number;
+  }[];
+  children: {
+    name: string;
+    birthDate: string;
+    roleId: number;
+  }[];
+}
