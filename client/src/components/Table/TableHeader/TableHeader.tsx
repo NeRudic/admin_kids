d15e731b14d39b32de93cba86227b59c93c0b1c1
Danil Holikov <break.build.one.minute@gmail.com>
2026-04-16 18:48:54 +0300

    feat: Create universal TableHeader.tsx component

--- client/src/components/Table/TableHeader/TableHeader.tsx
+++ client/src/components/Table/TableHeader/TableHeader.tsx
@@ -1,7 +1,55 @@
-export default function TableHeader() {
+import { useEffect, useRef, useState } from "react";
+
+interface ITableHeader {
+  button_label?: string;
+  placeholder: string;
+  onSearch: (value: string) => void;
+}
+
+export default function TableHeader({
+  button_label,
+  placeholder,
+  onSearch,
+}: ITableHeader) {
+  const [inputValue, setInputValue] = useState("");
+  const firstRender = useRef(true);
+
+  // Track the first render
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+    // Fetch debounce
+    const timeoutID = setTimeout(() => {
+      const data = inputValue.trim();
+      if (data.length >= 3 || data.length === 0) {
+        onSearch(data);
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutID);
+  }, [inputValue]);
+
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
+
   return (
     <div className="table_header">
-      <input type="text" />
+      <div className="search_area">
+        <div className="input_wrapper">
+          <input
+            type="text"
+            placeholder={placeholder}
+            onChange={onChangeHandler}
+            value={inputValue}
+          />
+          <div className="results"></div>
+          {button_label?.trim() && <button>{button_label}</button>}
+        </div>
+      </div>
+      <div className="title_area"></div>
     </div>
   );
 }
