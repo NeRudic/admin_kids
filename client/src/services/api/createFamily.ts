a8d9e40550970f134ef3036adf5cda1d69db914a
Danil Holikov <break.build.one.minute@gmail.com>
2026-04-20 18:57:07 +0300

    refactor(api): change errors on apiError()

--- client/src/services/api/createFamily.ts
+++ client/src/services/api/createFamily.ts
@@ -1,6 +1,7 @@
 import { createFamilyUrl } from "./api";
 import { NewFamilyInterface } from "../../.types";
 import axios from "axios";
+import { apiError } from "./api.error";
 
 export const createFamily = async (requestData: NewFamilyInterface) => {
   try {
@@ -10,15 +11,6 @@ export const createFamily = async (requestData: NewFamilyInterface) => {
     );
     console.log(`[Axios]Data sent: `, data);
   } catch (e) {
-    if (axios.isAxiosError(e)) {
-      if (e?.response) {
-        console.error("[Axios]Status:", e.response.status);
-        console.error("[Axios]Server message:", e.response.data.message);
-      }
-    } else if (e instanceof Error) {
-      console.error("[Error]Data sent failed! Error: ", e.message);
-    } else {
-      console.error("[Unknown]Unexpected error!");
-    }
+    apiError(e);
   }
 };
