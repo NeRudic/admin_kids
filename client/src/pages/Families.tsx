596c14b953ad949dd107f85ea3e61a985871beca
Danil Holikov <break.build.one.minute@gmail.com>
2026-04-25 13:44:35 +0300

    feat(pages): add data handling (WIP)

--- client/src/pages/Families.tsx
+++ client/src/pages/Families.tsx
@@ -1,18 +1,33 @@
-import { findFamilies } from "../services/api/findFamilies";
 import TableHeader from "../components/Table/TableHeader/TableHeader";
-import familiesMapper from "../services/families.mapper";
+import { useEffect, useState } from "react";
+import { IGrouppedFamily } from "../.types";
+import { familyDataPreparer } from "../services/families.data.utils";
 
 export default function Families() {
-  const dataNotifier = (data: string) => {
-    findFamilies(data);
+  const [inputValue, setInputValue] = useState("");
+  const [renderData, setRenderData] = useState<IGrouppedFamily[]>();
+
+  const dataNotifier = async (inputValue: string) => {
+    const preparedData = await familyDataPreparer(inputValue);
+    setRenderData(preparedData);
   };
 
+  useEffect(() => {
+    dataNotifier("");
+  }, []);
+
+  const state = { inputValue: inputValue, setInputValue: setInputValue };
   return (
     <>
-      <TableHeader placeholder="Введiть сiм'ю" dataNotifier={dataNotifier}>
+      <TableHeader
+        placeholder="Введiть сiм'ю"
+        state={state}
+        dataNotifier={dataNotifier}
+      >
         <h5>Сім'я</h5>
         <h5>Останнiй вiзит</h5>
       </TableHeader>
+      <div className="families_content"></div>
     </>
   );
 }
