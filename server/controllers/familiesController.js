755c96573f1c3add0be251436fa3b56b24ce4f5c
Danil Holikov <break.build.one.minute@gmail.com>
2026-05-02 12:51:50 +0300

    feat(api): add findFamily method

--- server/controllers/familiesController.js
+++ server/controllers/familiesController.js
@@ -1,14 +1,29 @@
-import { createFamilyService } from "../services/familiesService.js";
+import {
+  createFamilyService,
+  findFamilyService,
+} from "../services/familiesService.js";
 
 class FamiliesController {
   async createFamily(req, res) {
     try {
       const result = await createFamilyService({ ...req.body });
       res.status(201).json({ message: result });
-    } catch (e) {
-      res
-        .status(500)
-        .json({ message: `Family create has failed with error: ${e.message}` });
+    } catch (err) {
+      res.status(500).json({
+        message: `Family create has failed with error: ${err.message}`,
+      });
+    }
+  }
+
+  async findFamily(req, res) {
+    try {
+      const result = await findFamilyService(req.body);
+
+      res.status(200).json({ message: result });
+    } catch (err) {
+      res.status(500).json({
+        message: `Family find has failed with error: ${err.message}`,
+      });
     }
   }
 }
