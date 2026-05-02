18fd2026700b141fcd93097374406d9709a7455a
Danil Holikov <break.build.one.minute@gmail.com>
2026-05-02 12:43:17 +0300

    feat(validation): add FindFamilySchema

--- server/schemas/familySchema.js
+++ server/schemas/familySchema.js
@@ -6,7 +6,7 @@ export const FamilySchema = z.object({
   adults: z
     .array(
       z.object({
-        name: z.string().min(1, "Ім'я не може бути порожнім!"),
+        first_name: z.string().min(1, "Ім'я не може бути порожнім!"),
         phone: z
           .string()
           .regex(/^\+380\d{9}$/, "Неправильний формат телефону!"),
@@ -18,12 +18,16 @@ export const FamilySchema = z.object({
   children: z
     .array(
       z.object({
-        name: z.string().min(1, "Ім'я не може бути порожнім!"),
-        birthDate: z
+        first_name: z.string().min(1, "Ім'я не може бути порожнім!"),
+        birthday: z
           .string()
           .regex(/^\d{2}\.\d{2}\.\d{4}$/, "Неправильний формат дати!"),
         roleId: z.number().min(1, "Роль не може бути порожньою!"),
       }),
     )
     .min(1, "Має бути хоча б 1 дитина!"),
+});
+
+export const FindFamilySchema = z.object({
+  query: z.string().min(3, "Повинно бути хоча б 3 символи!"),
 });
