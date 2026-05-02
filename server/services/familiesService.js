d199003c53a2ac599e8a8057ef0aa9db975ea97f
Danil Holikov <break.build.one.minute@gmail.com>
2026-05-02 12:26:20 +0300

    feat(db): add find families service

--- server/services/familiesService.js
+++ server/services/familiesService.js
@@ -1,4 +1,5 @@
 import { db } from "../app.js";
+import { groupper } from "./utilities/groupper.js";
 
 export async function createFamilyService({ familyName, adults, children }) {
   try {
@@ -7,8 +8,7 @@ export async function createFamilyService({ familyName, adults, children }) {
 
     //Create a new family
     const familyResult = await db.run(
-      `
-      INSERT INTO family (family_name) VALUES (?)`,
+      `INSERT INTO family (family_name) VALUES (?)`,
       [familyName],
     );
 
@@ -18,7 +18,7 @@ export async function createFamilyService({ familyName, adults, children }) {
     for (const adult of adults) {
       const adultResult = await db.run(
         `INSERT INTO adult (first_name, family_id, role_id) VALUES (?, ?, ?)`,
-        [adult.name, familyId, adult.roleId],
+        [adult.first_name, familyId, adult.roleId],
       );
 
       const adultId = adultResult.lastID;
@@ -34,22 +34,61 @@ export async function createFamilyService({ familyName, adults, children }) {
     //Children
     for (const child of children) {
       await db.run(
-        `INSERT INTO children (child_name, child_birthday, role_id, family_id) VALUES (?, ?, ?, ?)`,
-        [child.name, child.birthDate, child.roleId, familyId],
+        `INSERT INTO child (first_name, birthday, role_id, family_id) VALUES (?, ?, ?, ?)`,
+        [child.first_name, child.birthday, child.roleId, familyId],
       );
     }
 
     await db.run(`COMMIT`);
 
     return { id: familyId, familyName: familyName };
-  } catch (error) {
+  } catch (err) {
     await db.run(`ROLLBACK`);
     // console.error("Database Error:", error.message);
-    throw error;
+    throw err;
   }
 }
 
-export async function findFamily() {}
+export async function findFamilyService({ query }) {
+  try {
+    const adultsFindResult = await db.all(
+      `SELECT a.first_name, a.id, f.family_name, f.id AS family_id, p.phone_number, ar.role
+      FROM adult AS a
+      JOIN family AS f ON a.family_id = f.id
+      JOIN phone AS p ON a.id = p.adult_id
+      JOIN adult_role AS ar ON a.role_id = ar.id
+      WHERE f.family_name LIKE ?`,
+      [`${query}%`],
+    );
+
+    console.log(adultsFindResult);
+
+    const childrenFindResult = await db.all(
+      `SELECT c.first_name, c.birthday, c.id, f.family_name, f.id AS family_id, cr.role
+      FROM child AS c
+      JOIN family AS f ON f.id = c.family_id
+      JOIN child_role AS cr ON c.role_id = cr.id
+      WHERE f.family_name LIKE ?`,
+      [`${query}%`],
+    );
+
+    console.log(childrenFindResult);
+
+    const adults = adultsFindResult.map((a) => ({
+      ...a,
+      type: "adults",
+    }));
+
+    const children = childrenFindResult.map((c) => ({
+      ...c,
+      type: "children",
+    }));
+
+    return groupper([...adults, ...children]);
+  } catch (err) {
+    throw err;
+  }
+}
 
 export async function editFamily() {}
 
