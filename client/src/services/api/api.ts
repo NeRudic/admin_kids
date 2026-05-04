90fb424b0fb92cd0c48fd49d1ff7bc1b29a3340c
Danil Holikov <break.build.one.minute@gmail.com>
2026-05-04 16:50:31 +0300

    fix(api): fix findFamiliesUrl to match backend

--- client/src/services/api/api.ts
+++ client/src/services/api/api.ts
@@ -1,4 +1,4 @@
 export const API_URL: string = "http://localhost:5115";
 
 export const createFamilyUrl: string = `${API_URL}/api/families/create_family/`;
-export const findFamiliesUrl: string = `${API_URL}/api/families/findFamilies/`;
+export const findFamiliesUrl: string = `${API_URL}/api/families/find_families/`;
