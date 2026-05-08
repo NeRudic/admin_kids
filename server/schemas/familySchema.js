34577fe9f300c4031698ae540723a4e45276b6c0
Danil Holikov <break.build.one.minute@gmail.com>
2026-05-08 18:31:40 +0300

    feat: capitalize query before search to match DB casing

--- server/schemas/familySchema.js
+++ server/schemas/familySchema.js
@@ -1,4 +1,4 @@
-import z from "zod";
+import z, { trim } from "zod";
 
 export const FamilySchema = z.object({
   familyName: z.string().min(1, "Прізвище не може бути порожнім!"),
@@ -29,5 +29,11 @@ export const FamilySchema = z.object({
 });
 
 export const FindFamilySchema = z.object({
-  query: z.string().min(3, "Повинно бути хоча б 3 символи!"),
+  query: z
+    .string()
+    .min(3, "Повинно бути хоча б 3 символи!")
+    .transform((val) => {
+      const clean = val.trim();
+      return clean[0].toUpperCase().concat(clean.slice(1));
+    }),
 });
