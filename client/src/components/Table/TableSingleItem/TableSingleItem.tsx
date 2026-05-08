9ef79effbf062df3d8d83289b456d9f8153f21c4
Danil Holikov <break.build.one.minute@gmail.com>
2026-05-08 15:46:55 +0300

    feat: add Button components, fix class names, apply grid layout

--- client/src/components/Table/TableSingleItem/TableSingleItem.tsx
+++ client/src/components/Table/TableSingleItem/TableSingleItem.tsx
@@ -2,6 +2,7 @@ import Svg from "../../svg/Svg";
 import { moreHandler } from "./setMoreHandler";
 import { ReactNode } from "react";
 import "./TableSingleItem.css";
+import Button from "../../Button/Button";
 
 interface ISTI {
   mainContent: ReactNode;
@@ -15,12 +16,25 @@ export default function TableSingleItem({ mainContent, moreContent }: ISTI) {
 
   return (
     <>
-      <div className="single_table_item msh-exl" onClick={setMoreHandler}>
-        <div className="eye_svg">{svg_eye()}</div>
-        <div className="table_content">{mainContent}</div>
-      </div>
-      <div className={`sti_more ${more ? "visible" : "hidden"}`}>
-        {moreContent}
+      <div className="tsi_wrapper">
+        <div
+          className={`table_single_item msh-bd ${more ? "tsi_active" : ""}`}
+          onClick={setMoreHandler}
+        >
+          <div className="eye_svg">{svg_eye()}</div>
+          <div className="table_content">{mainContent}</div>
+        </div>
+        <div className={`tsi_more msh-bd ${more ? "visible" : "hidden"}`}>
+          <div className="tsi_more_wrapper">
+            {moreContent}
+            {more && (
+              <div className="tsi_more_buttons_wrapper">
+                <Button classes="tsi_more_button edit mt-db">Редагувати</Button>
+                <Button classes="tsi_more_button delete mt-db">Видалити</Button>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
