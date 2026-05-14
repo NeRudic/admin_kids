bf6c223fbb9aefa14a9697e1a4aa8027a659f660
Danil Holikov <break.build.one.minute@gmail.com>
2026-05-14 18:36:12 +0300

    fix(api): rename id to roleId for correct app naming

--- server/services/rolesService.js
+++ server/services/rolesService.js
@@ -2,11 +2,11 @@ import { db } from "../app.js";
 
 export async function getRolesService() {
   const adultsRolesResult = await db.all(`
-      SELECT ar.id, ar.role FROM adult_role AS ar
+      SELECT ar.id AS roleId, ar.role FROM adult_role AS ar
     `);
 
   const childrenRolesResult = await db.all(`
-    SELECT cr.id, cr.role FROM child_role AS cr
+    SELECT cr.id AS roleId, cr.role FROM child_role AS cr
     `);
 
   return {
